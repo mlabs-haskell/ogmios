df37811a502d2368d2c32c611825650e9b3613a3
Szabo Gergely <gege251@mailbox.org>
2022-11-01 14:41:32 +0100

    Update nix flakes using cardano-haskell-packages

--- flake.nix
+++ flake.nix
@@ -2,97 +2,37 @@
   description = "ogmios";
 
   inputs = {
-    haskell-nix.url = "github:mlabs-haskell/haskell.nix";
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.hackage.follows = "hackage-nix";
+    };
 
     nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+
+    hackage-nix = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    iohk-nix = {
+      url = "github:input-output-hk/iohk-nix/cecab9c71d1064f05f1615eead56ac0b9196bc20";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
 
-    iohk-nix.url = "github:input-output-hk/iohk-nix/cecab9c71d1064f05f1615eead56ac0b9196bc20";
-
-    # all inputs below here are for use with haskell.nix
-    cardano-base = {
-      url = "github:input-output-hk/cardano-base/0f3a867493059e650cda69e20a5cbf1ace289a57";
-      flake = false;
-    };
-    cardano-crypto = {
-      url = "github:input-output-hk/cardano-crypto/f73079303f663e028288f9f4a9e08bcca39a923e";
-      flake = false;
-    };
-    cardano-ledger = {
-      url = "github:input-output-hk/cardano-ledger/ce3057e0863304ccb3f79d78c77136219dc786c6";
-      flake = false;
-    };
-    cardano-node = {
-      url = "github:input-output-hk/cardano-node/9f1d7dc163ee66410d912e48509d6a2300cfa68a";
-      flake = false;
-    };
-    cardano-prelude = {
-      url = "github:input-output-hk/cardano-prelude/bb4ed71ba8e587f672d06edf9d2e376f4b055555";
-      flake = false;
-    };
-    ekg-json = {
-      url = "github:vshabanov/ekg-json/00ebe7211c981686e65730b7144fbf5350462608";
-      flake = false;
-    };
-    flat = {
-      url = "github:input-output-hk/flat/ee59880f47ab835dbd73bea0847dab7869fc20d8";
-      flake = false;
-    };
-    goblins = {
-      url = "github:input-output-hk/goblins/cde90a2b27f79187ca8310b6549331e59595e7ba";
-      flake = false;
-    };
-    hedgehog-extras = {
-      url = "github:input-output-hk/hedgehog-extras/967d79533c21e33387d0227a5f6cc185203fe658";
-      flake = false;
-    };
-    hjsonpointer = {
-      url = "github:KtorZ/hjsonpointer/879f0e74d55eef76ceaec8f60ed07657ab84bad7";
-      flake = false;
-    };
-    hjsonschema = {
-      url = "github:KtorZ/hjsonschema/35e0b05c3867463363e67f00a5092cd39fa33313";
-      flake = false;
-    };
-    io-sim = {
-      url = "github:input-output-hk/io-sim/f4183f274d88d0ad15817c7052df3a6a8b40e6dc";
-      flake = false;
-    };
-    iohk-monitoring-framework = {
-      url = "github:input-output-hk/iohk-monitoring-framework/066f7002aac5a0efc20e49643fea45454f226caa";
-      flake = false;
-    };
-    optparse-applicative = {
-      url = "github:input-output-hk/optparse-applicative/7497a29cb998721a9068d5725d49461f2bba0e7a";
-      flake = false;
-    };
-    ouroboros-network = {
-      url = "github:input-output-hk/ouroboros-network/a65c29b6a85e90d430c7f58d362b7eb097fd4949";
-      flake = false;
-    };
-    plutus = {
-      url = "github:input-output-hk/plutus/f680ac6979e069fcc013e4389ee607ff5fa6672f";
-      flake = false;
-    };
-    typed-protocols = {
-      url = "github:input-output-hk/typed-protocols/181601bc3d9e9d21a671ce01e0b481348b3ca104";
-      flake = false;
-    };
-    wai-routes = {
-      url = "github:KtorZ/wai-routes/d74b39683792649c01113f40bf57724dcf95c96a";
-      flake = false;
-    };
-    Win32-network = {
-      url = "github:input-output-hk/Win32-network/3825d3abf75f83f406c1f7161883c438dac7277d";
+    # all inputs below here are for pinning with haskell.nix
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages/b074321c4c8cbf2c3789436ab11eaa43e1c441a7";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, haskell-nix, iohk-nix, ... }@inputs:
+  outputs = { self, nixpkgs, haskell-nix, iohk-nix, CHaP, ... }@inputs:
     let
       defaultSystems = [
         "x86_64-linux"
@@ -116,8 +56,11 @@
           pkgs = nixpkgsFor system;
           src = ./.;
         in
-        import ./nix {
+        import ./nix/default.nix {
           inherit src inputs pkgs system static;
+          inputMap = {
+            "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
+          };
         };
 
     in
